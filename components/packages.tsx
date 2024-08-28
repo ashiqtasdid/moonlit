6ad5b76883fb5ae88bd661439b9ac2f6e926f0e1
ashiqtasdid <ashiqtasdid5@gmail.com>
2024-08-28 13:45:39 +0600

    Removed Comments

--- components/packages.tsx
+++ components/packages.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React, { useRef } from "react";
+import React, { useRef, useState } from "react";
 import {
   motion,
   useMotionTemplate,
@@ -39,8 +39,14 @@ const Packages = () => {
 const ROTATION_RANGE = 32.5;
 const HALF_ROTATION_RANGE = ROTATION_RANGE / 2;
 
-const Basic: React.FC = () => {
+const CardTemplate: React.FC<{
+  imgSrc: string;
+  altText: string;
+  ringColor: string;
+  shadowColor: string;
+}> = ({ imgSrc, altText, ringColor, shadowColor }) => {
   const ref = useRef<HTMLDivElement>(null);
+  const [isHovered, setIsHovered] = useState(false);
 
   const x = useMotionValue(0);
   const y = useMotionValue(0);
@@ -70,6 +76,11 @@ const Basic: React.FC = () => {
   const handleMouseLeave = () => {
     x.set(0);
     y.set(0);
+    setIsHovered(false);
+  };
+
+  const handleMouseEnter = () => {
+    setIsHovered(true);
   };
 
   return (
@@ -77,11 +88,12 @@ const Basic: React.FC = () => {
       ref={ref}
       onMouseMove={handleMouseMove}
       onMouseLeave={handleMouseLeave}
+      onMouseEnter={handleMouseEnter}
       style={{
         transformStyle: "preserve-3d",
         transform,
       }}
-      className="relative h-[28rem] w-[22rem] mx-auto rounded-xl transition-transform hover:scale-105"
+      className={`relative h-[28rem] w-[22rem] mx-auto rounded-xl transition-transform hover:scale-105`}
     >
       <motion.div
         style={{
@@ -91,15 +103,18 @@ const Basic: React.FC = () => {
         className="absolute inset-0 flex items-center justify-center"
       >
         <motion.div
-          className="rounded-xl overflow-hidden ring-0 ring-emerald-400 ring-offset-2 ring-offset-gray-800 shadow-xl shadow-emerald-400/50"
-          whileHover={{ ringWidth: "6px", boxShadow: "0 0 15px #34D399" }}
+          className={`rounded-xl overflow-hidden ring-0 ${
+            isHovered ? `ring-4 ${ringColor}` : ""
+          } ring-offset-2 ring-offset-gray-800 shadow-xl ${
+            isHovered ? `shadow-${shadowColor}/50` : ""
+          }`}
           transition={{ duration: 0.3, ease: "easeInOut" }}
         >
           <Image
-            src="/assets/java.jpg"
+            src={imgSrc}
             width={350}
             height={200}
-            alt="JAVA"
+            alt={altText}
             className="object-cover"
           />
         </motion.div>
@@ -108,142 +123,31 @@ const Basic: React.FC = () => {
   );
 };
 
-const Premium: React.FC = () => {
-  const ref = useRef<HTMLDivElement>(null);
+const Basic = () => (
+  <CardTemplate
+    imgSrc="/assets/java.jpg"
+    altText="JAVA"
+    ringColor="ring-emerald-400"
+    shadowColor="emerald-400"
+  />
+);
 
-  const x = useMotionValue(0);
-  const y = useMotionValue(0);
+const Premium = () => (
+  <CardTemplate
+    imgSrc="/assets/bedrock.jpg"
+    altText="BEDROCK"
+    ringColor="ring-blue-500"
+    shadowColor="blue-500"
+  />
+);
 
-  const xSpring = useSpring(x, { stiffness: 200, damping: 20 });
-  const ySpring = useSpring(y, { stiffness: 200, damping: 20 });
-
-  const transform = useMotionTemplate`rotateX(${xSpring}deg) rotateY(${ySpring}deg)`;
-
-  const handleMouseMove = (e: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
-    if (!ref.current) return;
-
-    const rect = ref.current.getBoundingClientRect();
-    const width = rect.width;
-    const height = rect.height;
-
-    const mouseX = e.clientX - rect.left;
-    const mouseY = e.clientY - rect.top;
-
-    const rY = ((mouseX / width) * ROTATION_RANGE - HALF_ROTATION_RANGE) * 1;
-    const rX = ((mouseY / height) * ROTATION_RANGE - HALF_ROTATION_RANGE) * -1;
-
-    x.set(rX);
-    y.set(rY);
-  };
-
-  const handleMouseLeave = () => {
-    x.set(0);
-    y.set(0);
-  };
-
-  return (
-    <motion.div
-      ref={ref}
-      onMouseMove={handleMouseMove}
-      onMouseLeave={handleMouseLeave}
-      style={{
-        transformStyle: "preserve-3d",
-        transform,
-      }}
-      className="relative h-[28rem] w-[22rem] mx-auto rounded-xl transition-transform hover:scale-105"
-    >
-      <motion.div
-        style={{
-          transform: "translateZ(50px)",
-          transformStyle: "preserve-3d",
-        }}
-        className="absolute inset-0 flex items-center justify-center"
-      >
-        <motion.div
-          className="rounded-xl overflow-hidden ring-0 ring-blue-500 ring-offset-2 ring-offset-gray-800 shadow-xl shadow-blue-500/50"
-          whileHover={{ ringWidth: "6px", boxShadow: "0 0 15px #3B82F6" }}
-          transition={{ duration: 0.3, ease: "easeInOut" }}
-        >
-          <Image
-            src="/assets/bedrock.jpg"
-            width={350}
-            height={200}
-            alt="BEDROCK"
-            className="object-cover"
-          />
-        </motion.div>
-      </motion.div>
-    </motion.div>
-  );
-};
-
-const Mixed: React.FC = () => {
-  const ref = useRef<HTMLDivElement>(null);
-
-  const x = useMotionValue(0);
-  const y = useMotionValue(0);
-
-  const xSpring = useSpring(x, { stiffness: 200, damping: 20 });
-  const ySpring = useSpring(y, { stiffness: 200, damping: 20 });
-
-  const transform = useMotionTemplate`rotateX(${xSpring}deg) rotateY(${ySpring}deg)`;
-
-  const handleMouseMove = (e: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
-    if (!ref.current) return;
-
-    const rect = ref.current.getBoundingClientRect();
-    const width = rect.width;
-    const height = rect.height;
-
-    const mouseX = e.clientX - rect.left;
-    const mouseY = e.clientY - rect.top;
-
-    const rY = ((mouseX / width) * ROTATION_RANGE - HALF_ROTATION_RANGE) * 1;
-    const rX = ((mouseY / height) * ROTATION_RANGE - HALF_ROTATION_RANGE) * -1;
-
-    x.set(rX);
-    y.set(rY);
-  };
-
-  const handleMouseLeave = () => {
-    x.set(0);
-    y.set(0);
-  };
-
-  return (
-    <motion.div
-      ref={ref}
-      onMouseMove={handleMouseMove}
-      onMouseLeave={handleMouseLeave}
-      style={{
-        transformStyle: "preserve-3d",
-        transform,
-      }}
-      className="relative h-[28rem] w-[22rem] mx-auto rounded-xl transition-transform hover:scale-105"
-    >
-      <motion.div
-        style={{
-          transform: "translateZ(50px)",
-          transformStyle: "preserve-3d",
-        }}
-        className="absolute inset-0 flex items-center justify-center"
-      >
-        <motion.div
-          className="rounded-xl overflow-hidden ring-0 ring-purple-500 ring-offset-2 ring-offset-gray-800 shadow-xl shadow-purple-500/50"
-          whileHover={{ ringWidth: "6px", boxShadow: "0 0 15px #8B5CF6" }}
-          transition={{ duration: 0.3, ease: "easeInOut" }}
-        >
-          <Image
-            src="/assets/bedrock.jpg"
-            width={350}
-            height={200}
-            alt="MIXED"
-            className="object-cover"
-          />
-        </motion.div>
-      </motion.div>
-    </motion.div>
-  );
-};
+const Mixed = () => (
+  <CardTemplate
+    imgSrc="/assets/bedrock.jpg"
+    altText="MIXED"
+    ringColor="ring-purple-500"
+    shadowColor="purple-500"
+  />
+);
 
 export default Packages;
